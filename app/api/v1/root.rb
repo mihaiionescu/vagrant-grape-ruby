18d60d4c5a986bfc60144d07b02c9ec11021432a
Mihai Ionescu <blueicedeliverman@gmail.com>
2015-02-17 03:54:39 +0000

    Added method for generating access_tokens.

--- app/api/v1/root.rb
+++ app/api/v1/root.rb
@@ -8,6 +8,14 @@ class Dummy::API::V1::Root < ::Grape::API
     }
   end
 
+  get "/token" do
+    auth_token = AccessToken.create!
+    {
+      status: 201,
+      data: auth_token.token
+    }
+  end
+
   get "/ping_auth" do
     env["warden"].authenticate!
     'authenticated endpoint'
