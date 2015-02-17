c24db38b5f8974a48d41f4b01e8f1a96f51b4005
Mihai Ionescu <blueicedeliverman@gmail.com>
2015-02-17 03:07:01 +0000

    AR initialiser uses symbol to establish connection.

--- config/initializers/ar.rb
+++ config/initializers/ar.rb
@@ -6,6 +6,6 @@ config_dir = Dummy::Application.config.root
 
 ActiveRecord::Base.configurations = YAML.load(File.read(File.join(config_dir, 'config', 'database.yml')))
 
-ActiveRecord::Base.establish_connection env
+ActiveRecord::Base.establish_connection env.to_sym
 
 ActiveRecord::Base.logger = Logger.new(File.open(File.join(config_dir, 'log', "#{env}.log"), 'a+'))
