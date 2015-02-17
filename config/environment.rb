9468aef988ebfc7a02af9a5637780187fd79bffc
Mihai Ionescu <blueicedeliverman@gmail.com>
2015-02-17 16:23:39 +0000

    Deploy - use symbol for the environment name.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-env = (ENV['RACK_ENV'] || :development)
+env = (ENV['RACK_ENV'] || :development).to_sym
 
 require 'bundler'
 Bundler.require :default, env.to_sym
