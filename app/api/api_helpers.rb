9a529ad5a365fc4b31e7e9d54648127cb0c4aead
Mihai Ionescu <blueicedeliverman@gmail.com>
2015-02-17 14:51:02 +0000

    Moved authentication to API helper file.

--- app/api/api_helpers.rb
+++ app/api/api_helpers.rb
@@ -1,4 +1,17 @@
 module Dummy::API::Helpers
+  def authenticate!
+    at = warden.authenticate!
+    @current_token ||= at
+  end
+
+  def warden
+    env['warden']
+  end
+
+  def current_token
+    authenticate! if @current_token.blank?
+  end
+
   def not_found!
     error! data(404, 'Not found'), 404
   end
