2a8cf63f8fef31ce3f8e9c71cad4f0d0534fa2f3
Mihai Ionescu <blueicedeliverman@gmail.com>
2015-02-17 16:22:17 +0000

    Deploy - revert to copying local code for now.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,14 +12,14 @@ end
 
 # Use a simple directory tree copy here to make demo easier.
 # You probably want to use your own repository for a real app
-ssh_options[:forward_agent] = true #optional : for deploy from github
-set :repository, "git@github.com:mihaiionescu/vagrant-rubber-env.git"
-set :scm, "git"
-set :deploy_via, :remote_cache
-set :branch, 'master'
-# set :scm, :none
-# set :repository, "."
-# set :deploy_via, :copy
+# ssh_options[:forward_agent] = true #optional : for deploy from github
+# set :repository, "git@github.com:mihaiionescu/vagrant-rubber-env.git"
+# set :scm, "git"
+# set :deploy_via, :remote_cache
+# set :branch, 'master'
+set :scm, :none
+set :repository, "."
+set :deploy_via, :copy
 
 # Easier to do system level config as root - probably should do it through
 # sudo in the future.  We use ssh keys for access, so no passwd needed
